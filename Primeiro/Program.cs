d829d6f55586b85c74dc6748b40ee2a1fcf4b845
Queiroz <jmfqueiroz@gmail.com>
2020-03-31 09:06:18 +0100

    Projeto teste - criacao

---
+++ Primeiro/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Primeiro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            List<string> list = new List<string>();
+
+            list.Add("M1");
+            list.Add("A");
+            list.Add("B");
+            list.Add("C");
+            list.Insert(2, "M2");
+
+            foreach (string obj in list)
+            {
+                Console.WriteLine(obj);
+            }
+
+            Console.WriteLine("#List:" + list.Count);
+
+            string s1 = list.Find(x => x[0] == 'M');
+            string s2 = list.FindLast(x => x[0] == 'M');
+
+            Console.WriteLine(s1);
+            Console.WriteLine(s2);
+        }
+    }
+}
